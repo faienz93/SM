ba603b383aa8c733fc1a8378383c9d6e6d1a41d4
antoniofaienza93 <afaienza93@gmail.com>
2018-11-14 19:23:33 +0100

    [UPDATE] Set Bologna Coordinates

--- js/init.js
+++ js/init.js
@@ -20,4 +20,18 @@
             $("body").css("backgroundColor","white");
         }
     });
+
+
+    var map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM() // Tiled Layer
+          })
+        ],
+        view: new ol.View({
+          center: ol.proj.fromLonLat([11.327591, 44.498955]), // Longitude and Latitude 
+          zoom: 12
+        })
+      });
 });
