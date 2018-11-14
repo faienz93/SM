643a9fc1d5be9b049938548058d1bd739ecae291
antoniofaienza93 <afaienza93@gmail.com>
2018-11-14 13:39:52 +0100

    [UPDATE] Change sidebar with W3School

--- js/init.js
+++ js/init.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
+
     $('#buttonSidebar').click(function(){
-        $('.sidebar').toggleClass('fliph');
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
     });
+
+    // function openNav() {
+    //     document.getElementById("mySidenav").style.width = "250px";
+    //     document.getElementById("main").style.marginLeft = "250px";
+    //     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    // }
+    
+    // function closeNav() {
+    //     document.getElementById("mySidenav").style.width = "0";
+    //     document.getElementById("main").style.marginLeft= "0";
+    //     document.body.style.backgroundColor = "white";
+    // }
       
  });
