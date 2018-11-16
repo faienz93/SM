c69211c54f799b9b2b32315c5a1bff590fb4bb19
antoniofaienza93 <afaienza93@gmail.com>
2018-11-16 18:20:56 +0100

    Continuation of Integration Bing Maps

--- js/sidebar.js
+++ js/sidebar.js
@@ -13,6 +13,23 @@
  */
 function sidebar() {
 
+    // When click on the hamburger icon (Thanks the appropriate library) it open and close the 
+    // sidebar
+    $('#hambergerButton').click(function () {
+        $(this).toggleClass('is-active');
+        var checkClass = $("#hambergerButton").hasClass('is-active');
+        if (checkClass) {
+            $("#mySidenav").css("width", "250px");
+            $("#main").css("marginLeft", "250px");
+            $("body").css("backgroundColor", "rgba(0,0,0,0.4)");
+        } else {
+            $("#mySidenav").css("width", "0");
+            $("#main").css("marginLeft", "0");
+            $("body").css("backgroundColor", "white");
+        }
+    });
+
+
     //Loop through all dropdown buttons to toggle between hiding and showing its dropdown content
     //This allows the user to have multiple dropdowns without any conflict 
     //REF: https://www.w3schools.com/howto/howto_js_dropdown_sidenav.asp
@@ -42,25 +59,6 @@ function sidebar() {
     //     });
     // }
 
-    // When click on the hamburger icon (Thanks the appropriate library) it open and close the 
-    // sidebar
-    $('#hambergerButton').click(function () {
-        $(this).toggleClass('is-active');
-        var checkClass = $("#hambergerButton").hasClass('is-active');
-        if (checkClass) {
-            $("#mySidenav").css("width", "250px");
-            $("#main").css("marginLeft", "250px");
-            $("body").css("backgroundColor", "rgba(0,0,0,0.4)");
-        } else {
-            $("#mySidenav").css("width", "0");
-            $("#main").css("marginLeft", "0");
-            $("body").css("backgroundColor", "white");
-        }
-    });
-
-
-
-
-
+    
 
 }
