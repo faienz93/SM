a245c0ff603484add290680bbb7c060314b17b46
antoniofaienza93 <afaienza93@gmail.com>
2018-11-26 13:07:57 +0100

    Integrate Hamburger

--- js/navbar.js
+++ js/navbar.js
@@ -16,30 +16,35 @@ function navbar(){
         $(this).toggleClass('is-active');
         var checkClass = $("#hambergerButton").hasClass('is-active');
         if (checkClass) {
-            $("#mySidenav").css("width", "250px");
-            $("#main").css("marginLeft", "250px");
-            $("body").css("backgroundColor", "rgba(0,0,0,0.4)");
+            // $("#mySidenav").css("width", "250px");
+            // $("#main").css("marginLeft", "250px");
+            // $("body").css("backgroundColor", "rgba(0,0,0,0.4)");
+            $('#sidebar').toggleClass('active');
+           
         } else {
-            $("#mySidenav").css("width", "0");
-            $("#main").css("marginLeft", "0");
-            $("body").css("backgroundColor", "white");
+            // $("#mySidenav").css("width", "0");
+            // $("#main").css("marginLeft", "0");
+            // $("body").css("backgroundColor", "white");
+            $('#sidebar').toggleClass('active');
+          
         }
     });
 
 
-    var lastClicked;
-    var a = $('.dropdown-menu').find("a");
-    // Set None as Default choice
-    // a.first().css("background","#c9c9c9");
-    // a.first().css("color","#000000");
-    // lastClicked = a;
-    a.click(function(){
-        if(lastClicked!=undefined){
-            lastClicked.css("background","none");
-            lastClicked.css("color","#818181");           
-        }
-        $(this).css("background","#c9c9c9");
-        $(this).css("color","#000000");       
-        lastClicked =  $(this);
-    })
+
+    // var lastClicked;
+    // var a = $('.dropdown-menu').find("a");
+    // // Set None as Default choice
+    // // a.first().css("background","#c9c9c9");
+    // // a.first().css("color","#000000");
+    // // lastClicked = a;
+    // a.click(function(){
+    //     if(lastClicked!=undefined){
+    //         lastClicked.css("background","none");
+    //         lastClicked.css("color","#818181");           
+    //     }
+    //     $(this).css("background","#c9c9c9");
+    //     $(this).css("color","#000000");       
+    //     lastClicked =  $(this);
+    // })
 }
