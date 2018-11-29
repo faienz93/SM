b30fda5f95a9e9325527a0e98f22e76617a02726
antoniofaienza93 <afaienza93@gmail.com>
2018-11-29 16:43:31 +0100

    Update login for registration

--- routes/login.js
+++ routes/login.js
@@ -3,12 +3,16 @@
 
 var express = require('express');
 const mongoose = require('mongoose');
-var app     = express();
-var path    = require('path');
+var app = express();
+var path = require('path');
+const auth = require('http-auth');
 
 const router = express.Router();
 const Registration = mongoose.model('Registration');
 
+const basic = auth.basic({
+  file: path.join(__dirname, '../users.htpasswd'),
+});
 
 // define reading of stating file
 // app.use(express.static(path.join(__dirname, '/')));
@@ -19,19 +23,31 @@ const Registration = mongoose.model('Registration');
 //   });
 
 
-  // app.get('/',function(req,res){
-  //   res.sendFile('../views/index.html');
-  //   //__dirname : It will resolve to your project folder.
-  // });
+// app.get('/',function(req,res){
+//   res.sendFile('../views/index.html');
+//   //__dirname : It will resolve to your project folder.
+// });
 
-  app.post('/login', function(req, res) {
-    console.log(req.body);
-    const registration = new Registration(req.body);
-    registration.save()
-      .then(() => { res.send('Thank you for your registration!'); })
+router.post('/login', function (req, res) {
+  console.log(req.body);
+  const registration = new Registration(req.body);
+  registration.save()
+    .then(() => { res.send('Thank you for your registration!'); })
+    .catch(() => { res.send('Sorry! Something went wrong.'); });
+  // res.send('Name: ' + req.body.name +  " mail " + req.body.email + ".");
+});
+
+
+router.get('/registrations', auth.connect(basic), (req, res) => {
+    Registration.find()
+      .then((registrations) => {  res.send(registrations)
+        })
       .catch(() => { res.send('Sorry! Something went wrong.'); });
-    // res.send('Name: ' + req.body.name +  " mail " + req.body.email + ".");
-  });
+});
 
 
-module.exports = app;
+
+
+
+
+module.exports = router;
