97ad9c2f735f0cc78b7caa94f5676ef5be8906e8
antoniofaienza93 <afaienza93@gmail.com>
2018-11-29 13:55:25 +0100

    [ADD] Data Registration Scheme

---
+++ models/registration.js
@@ -0,0 +1,11 @@
+/*
+ * ===========================================================================
+ * File: Registration.js 
+ * Author: Antonio Faienza
+ * Desc: This file allow to defines the Database Schema. Schemas allow you to 
+ * define the fields stored in each document along with their type, validation
+ * requirements and default values. 
+ * ===========================================================================
+ */
+
+ const mongose = require('mongose');
