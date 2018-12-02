2609de2c2e392ef4a9cc935bcc30ea6602dee791
antoniofaienza93 <afaienza93@gmail.com>
2018-12-02 17:12:17 +0100

    add function that handle forms

--- js/init.js
+++ js/init.js
@@ -12,35 +12,58 @@ $(document).ready(function () {
 
   // Setting navbar
   navbar();
-  
+
   // Setting the sidebar
   sidebar();
 
-  // Setting the map 
-  map(); 
+  // reset form
+  handleForm();
 
+  // Setting the map 
+  map();
+
+
+});
+
+
+/**
+ * This function aims to bind event to reset buttons
+ * @method handleForm
+ */
+function handleForm() {
+  // handle show div
   $('.swapDiv').on("click", function () {
     var operation = $(this).attr("value");
-    if(operation==="addUser"){
-      $(".mainDiv").each(function() {
-        $(this).css("display","none");
+    if (operation === "addUser") {
+      $(".mainDiv").each(function () {
+        $(this).css("display", "none");
       });
-      $('#addUserDiv').css("display","block");
-    }else if(operation==="updateUser"){
-      $(".mainDiv").each(function() {
-        $(this).css("display","none");
+      $('#addUserDiv').css("display", "block");
+    } else if (operation === "updateUser") {
+      $(".mainDiv").each(function () {
+        $(this).css("display", "none");
       });
-      $('#updateyUserDiv').css("display","block");
-    }else if(operation==="deleteUser"){
-      $(".mainDiv").each(function() {
-        $(this).css("display","none");
+      $('#updateyUserDiv').css("display", "block");
+    } else if (operation === "deleteUser") {
+      $(".mainDiv").each(function () {
+        $(this).css("display", "none");
       });
-      $('#deleteUserDiv').css("display","block");
-    }else {
-      $(".mainDiv").each(function() {
-        $(this).css("display","none");
+      $('#deleteUserDiv').css("display", "block");
+    } else {
+      $(".mainDiv").each(function () {
+        $(this).css("display", "none");
       });
-      $('#map').css("display","block");
+      $('#map').css("display", "block");
     }
   });
-});
+
+  // Reset value of form Add User 
+  $('#resetAddUser').click(function () {
+    $('#addUserForm')[0].reset();
+  });
+
+  // Reset value of form Update User 
+  $('#resetUpdateUser').click(function () {
+    $('#updateUserForm')[0].reset();
+  });
+}
