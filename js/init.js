4d319e9b921a47e792fc7397a3882c8e91ceb774
antoniofaienza93 <afaienza93@gmail.com>
2018-12-06 17:49:25 +0100

    Refactor update form

--- js/init.js
+++ js/init.js
@@ -76,6 +76,17 @@ function handleForm() {
  */
 function populateFormUpdate(){
   $('select').on('change', function() {
+
+    // at the start all field are disabled. Then when i choice from dropdown becomes enabled
+    $('#usernameUserFormUpdate').removeAttr("disabled");
+    $('#emailUserFormUpdate').removeAttr("disabled");
+    $('#passwordUserFormUpdate').removeAttr("disabled");
+    $('#confirmPasswordUserFormUpdate').removeAttr("disabled"); 
+    $('#sendUserFormUpdate').removeAttr("disabled");
+    $("#resetUpdateUser").removeAttr("disabled");
+  
+
+    // set the field with the information of value from dropdown
     var val = jQuery.parseJSON( this.value );  
     $('#idUserFormUpdate').val(val._id);
     $('#usernameUserFormUpdate').val(val.username);
