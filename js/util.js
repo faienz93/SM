bfc467a386f9454ecb9392a13b60d1aa99254318
antoniofaienza93 <afaienza93@gmail.com>
2018-12-11 18:15:12 +0100

    [ADD] Method for retrieve url param

--- js/util.js
+++ js/util.js
@@ -121,11 +121,23 @@ function formSubmit(form,message = "Are you sure? "){
             }
         });
         return false;
-
- 
     
 }
 
+// https://stackoverflow.com/a/21903119/4700162
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
 
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
 
 
