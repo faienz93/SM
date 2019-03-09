a0cd5146b251d999152010e0404d1c3c727156ee
antoniofaienza93 <afaienza93@gmail.com>
2019-03-09 17:11:51 +0100

    [UPDATE] .gitignore

--- models/experiment.js
+++ models/experiment.js
@@ -1,6 +1,6 @@
 /*
  * ===========================================================================
- * File: Test.js 
+ * File: Experiments.js 
  * Author: Antonio Faienza
  * Desc: This file allow to defines the Database Schema. Schemas allow you to 
  * define the fields stored in each document along with their type, validation
